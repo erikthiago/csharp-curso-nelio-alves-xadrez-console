2d563328affb0174f84a53715fcd33b5e07b64c1
Erik Sousa <erik.sousa@outlook.com>
2020-02-24 20:44:02 -0300

    Adicionar arquivos de projeto.

---
+++ xadrez-console.tabuleiro/Cor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace xadrez_console.tabuleiro
+{
+    public enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
---
+++ xadrez-console.tabuleiro/TabuleiroException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez_console.tabuleiro
+{
+    public class TabuleiroException : Exception
+    {
+
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
